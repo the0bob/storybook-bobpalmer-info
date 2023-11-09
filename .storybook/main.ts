951a22c86b1e09a9156dbeac5fb9e1b544907aef
Guy <the0bob@users.noreply.github.com>
2023-11-09 09:18:25 -0600

    gh pages deploy, switch from vite to next

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,4 @@
-import type { StorybookConfig } from "@storybook/react-vite";
+import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
   stories: [
@@ -12,7 +12,7 @@ const config: StorybookConfig = {
     "@storybook/addon-interactions",
   ],
   framework: {
-    name: "@storybook/react-vite",
+    name: "@storybook/nextjs",
     options: {},
   },
   docs: {
