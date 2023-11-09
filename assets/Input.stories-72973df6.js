6d5046381dff09fbf924cfca687941f9cce7a10a
Guy <the0bob@users.noreply.github.com>
2023-11-09 08:56:11 -0600

    Updates

---
+++ assets/Input.stories-72973df6.js
@@ -0,0 +1,6 @@
+import{I as a}from"./Input-36b0dd67.js";import"./jsx-runtime-29545a09.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";const m={title:"Components/Input",component:a,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{backgroundColor:{control:"color"}}},o={args:{placeholder:"Input"}};var r,e,t;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    placeholder: 'Input'
+  }
+}`,...(t=(e=o.parameters)==null?void 0:e.docs)==null?void 0:t.source}}};const l=["Default"];export{o as Default,l as __namedExportsOrder,m as default};
+//# sourceMappingURL=Input.stories-72973df6.js.map
